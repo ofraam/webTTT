e6e5447d06156ab634f85ffbdb4479612e66558e
Ofra Amir <oamir@seas.harvard.edu>
2017-05-31 18:29:01 -0400

    added confidence, started adding timer
    timer not yet working

--- public_html/tictactoe/tictactoe.js
+++ public_html/tictactoe/tictactoe.js
@@ -61,7 +61,7 @@ E.board6_2_b = {
     ],
     nextPlayer: 1,
     streak:4,
-    turns: 3
+    turns: 5
 }
 
 E.board10_1 = {
@@ -190,6 +190,8 @@ function run_block() {
                 E.configuration = E.board10_2;
                 break;
         }
+
+
     }
 
 	 // var init = {
@@ -351,9 +353,21 @@ function submit_solution() {
 
 	var move = $("#bestmove").val();
 	E.move = move;
-	var solution = $("#solution").val();
-	servlog("best_move", move);
-	servlog("solution", solution);
+    var conf = $('input[name=confidence]:checked', '#experiment').val()
+    // if(typeof conf != 'undefined')
+	// {
+        // var solution = $("#solution").val();
+        servlog("best_move", move);
+        servlog("confidence", conf);
+	// }
+	// else {
+     //    alert("Please answer what your level of confidence in the solution is")
+     //    onContinue.curPage = 2;
+     //    onContinue();
+	// }
+
+
+	// servlog("solution", solution);
 }
 
 function suggest_solution(){
@@ -415,7 +429,7 @@ function onContinue() {
 			E.startTime=msTime();
 			$("#instructions.page").show()
 			$("#btnContinue").html('Continue to quiz')
-
+            $(window).scrollTop(0,0);
 			break;
 
 		case 4:
@@ -454,31 +468,58 @@ function onContinue() {
                 $('.turns').text(parseInt(E.configuration.turns));
                 $('.streak').text(parseInt(E.configuration.streak));
 				$("#experiment.page").show()
+                // Update the count down every 1 second
+                var x = setInterval(function() {
+
+                    // Get todays date and time
+                    var now = new Date().getTime();
+
+                    // Find the distance between now an the count down date
+                    var distance = countDownDate - now;
+
+                    // Time calculations for days, hours, minutes and seconds
+                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                    // Display the result in the element with id="demo"
+                    document.getElementById("timer").innerHTML =  minutes + "m " + seconds + "s ";
+
+                    // If the count down is finished, write some text
+                    if (distance < 0) {
+                        clearInterval(x);
+                        document.getElementById("timer").innerHTML = "EXPIRED";
+                    }
+                }, 1000);
                 $(window).scrollTop(0,0);
 
             }
 
 			break;
 
+		// case 6:
+        //
+		// 	// show_page_real();
+		// 	E.endTime=msTime();
+		// 	var timeSolution = E.endTime-E.startTime
+		// 	servlog("timeSolution", timeSolution);
+		// 	submit_solution();
+		//
+		// 	E.startTime = msTime();
+		// 	suggest_solution();
+		//
+		// 	break;
+			
 		case 6:
-
-			// show_page_real();
-			E.endTime=msTime();
-			var timeSolution = E.endTime-E.startTime
-			servlog("timeSolution", timeSolution);
-			submit_solution();
-			
-			E.startTime = msTime();
-			suggest_solution();
-				
-			break;
-			
-		case 7:
-			log_vote();
+			// log_vote();
 			
 			E.endTime = msTime()
-			var timeVote = E.endTime - E.startTime
-			servlog("timeVote", timeVote)
+            var timeSolution = E.endTime-E.startTime
+            submit_solution();
+            servlog("timeSolution", timeSolution);
+			// var timeVote = E.endTime - E.startTime
+			// servlog("timeVote", timeVote)
 			show_page_final()	
 
 	}
