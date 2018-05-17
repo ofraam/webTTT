c51aee6194280d514658b82ccd8cd2bcb316291c
ofraam <oamir@seas.harvard.edu>
2018-05-17 17:30:01 +0300

    time plots done

--- public_html/tictactoe/ttt-viewer.js
+++ public_html/tictactoe/ttt-viewer.js
@@ -11,6 +11,7 @@ E.widget = {};
 E.curr_point = {};
 E.curr_point_index = {};
 E.myPlot = {};
+E.myPlot_times = {};
 
 function unpack(rows, key) {
     return rows.map(function (row) {
@@ -105,11 +106,21 @@ function draw_board() {
     E.widget.run()
     var count = E.myPlot.data[0].x.length;
     colors = []
+    colors_times = []
     sizes = []
     ind = 0
     for (i=0;i<count;i++) {
-        colors.push('#00000');
-        sizes.push(10);
+        if (parseInt(E.curr_user_clicks[i].player) == 2)
+        {
+            colors.push('#000000');
+            colors_times.push('#000000');
+        }
+        else
+        {
+            colors.push('#17BECF');
+            colors_times.push('#cc6699');
+        }
+        sizes.push(6);
         if (E.myPlot.data[0].x[i] == E.curr_point) {
             ind = i;
             E.curr_point_index = i;
@@ -118,7 +129,9 @@ function draw_board() {
     colors[ind] = '#C54C82';
     sizes[ind] = 16;
     var update = {'marker':{color: colors, size:sizes}};
+    var update_times = {'marker':{color: colors_times, size:sizes}};
     Plotly.restyle('myDiv',update,[0]);
+    Plotly.restyle('myDiv',update_times,[1]);
 }
 
 function draw_chart() {
@@ -136,7 +149,7 @@ function draw_chart() {
 
         if (solved!='all') {
             E.curr_data = E.data.filter(function (el) {
-                return (el.board_name == board & el.action == 'click' & el.solved == solved)
+                return (el.board_name == board &  el.solved == solved)
             });
         }
         E.users = unpack(E.curr_data,'userid');
@@ -147,6 +160,16 @@ function draw_chart() {
             return (el.userid == E.users[E.curr_user_index] & el.action == 'click')
         });
 
+        var action_times = unpack(E.curr_user_clicks,'time_from_action');
+        max_time = parseInt(action_times[0])
+        for (i=0;i<action_times.length;i++){
+            action_times[i] = parseInt(action_times[i])
+            if (action_times[i]>max_time)
+            {
+                max_time=action_times[i]
+            }
+        }
+
         var resets_user = E.curr_data.filter(function (el) {
             return (el.userid == E.users[E.curr_user_index] & el.action == 'reset')
         });
@@ -155,15 +178,31 @@ function draw_chart() {
             shapes.push({'type': 'line',
                 // 'x0': unpack([resets_user[0]], 'time_rel_sec'),
                 'x0': resets_user[i].time_rel_sec,
-                'y0': -110,
+                'y0': 0,
                 'x1': resets_user[i].time_rel_sec,
-                'y1': 110,
+                'y1': max_time,
+                'yref': 'y2',
                 'line': {
                     'color': 'rgb(50, 171, 96)',
                     'width': 4,
                     'dash': 'dashdot',
                 },})
         }
+
+    for (i=0;i<resets_user.length;i++) {
+        shapes.push({'type': 'line',
+            // 'x0': unpack([resets_user[0]], 'time_rel_sec'),
+            'x0': resets_user[i].time_rel_sec,
+            'y0': -110,
+            'x1': resets_user[i].time_rel_sec,
+            'y1': 110,
+            'yref': 'y',
+            'line': {
+                'color': 'rgb(50, 171, 96)',
+                'width': 4,
+                'dash': 'dashdot',
+            },})
+    }
 
         var undos_user = E.curr_data.filter(function (el) {
             return (el.userid == E.users[E.curr_user_index] & el.action == 'undo')
@@ -173,9 +212,10 @@ function draw_chart() {
         shapes.push({'type': 'line',
             // 'x0': unpack([resets_user[0]], 'time_rel_sec'),
             'x0': undos_user[i].time_rel_sec,
-            'y0': -110,
+            'y0': 0,
             'x1': undos_user[i].time_rel_sec,
-            'y1': 110,
+            'y1': max_time,
+            'yref': 'y2',
             'line': {
                 'color': 'rgb(255, 153, 0)',
                 'width': 4,
@@ -183,33 +223,64 @@ function draw_chart() {
             },})
     }
 
+    for (i=0;i<undos_user.length;i++) {
+        shapes.push({'type': 'line',
+            // 'x0': unpack([resets_user[0]], 'time_rel_sec'),
+            'x0': undos_user[i].time_rel_sec,
+            'y0': -110,
+            'x1': undos_user[i].time_rel_sec,
+            'y1': 110,
+            'yref': 'y',
+            'line': {
+                'color': 'rgb(255, 153, 0)',
+                'width': 4,
+                'dash': 'dashdot',
+            },})
+    }
+
+
     var trace1 = {
             type: "scatter",
             mode: "lines+markers",
             x: unpack(E.curr_user_clicks, 'time_rel_sec'),
             y: unpack(E.curr_user_clicks, 'score_heuristic_x'),
-            line: {color: '#17BECF'}
-        }
+            line: {color: '#17BECF'},
+        name:'score heuristic x',
+        };
+
+    var trace2 = {
+        type: "scatter",
+        mode: "lines+markers",
+        x: unpack(E.curr_user_clicks, 'time_rel_sec'),
+        y: unpack(E.curr_user_clicks, 'time_from_action'),
+        line: {color: '#cc6699'},
+        yaxis: 'y2',
+        name:'delta time',
+    };
 
 
-        var data = [trace1];
+        var data = [trace1, trace2];
+        // var data_times = [trace2];
 
        var layout = {
-           title: E.users[E.curr_user_index] + '_'+ E.curr_data[0].solved + '_'+ E.curr_data[0].heuristic,
+           title: E.users[E.curr_user_index] + '_'+ E.curr_user_clicks[0].solved + '_'+ E.curr_user_clicks[0].heuristic,
            // yaxis: {
            //     type: 'log',
            //     autorange: true
            // }
-           'shapes':   shapes
-       };
+           'shapes':   shapes,
+           yaxis: {domain: [0, 0.65]},
+           yaxis2:{domain: [0.75, 1]}
+};
         E.curr_point = E.curr_user_clicks[0].time_rel_sec;
         E.curr_point_index = 0;
 
-        E.myPlot = document.getElementById('myDiv')
+        E.myPlot = document.getElementById('myDiv');
+        // E.myPlot_times = document.getElementById('myDivTimes');
 
         Plotly.newPlot('myDiv', data, layout);
 
-
+        // Plotly.newPlot('myDivTimes', data_times, layout);
 
        E.myPlot.on('plotly_click', function(data){
        var pts = '';
